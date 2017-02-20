5e28843753c21949dee27a6559a8c0a182be6f54
ljx <linjinxing@camera360.com>
2017-02-20 18:09:35 +0800

    [ljx] init

---
+++ TXCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TXCollectionView
+//
+//  Created by linjinxing on 17/2/20.
+//  Copyright © 2017年 tongxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
